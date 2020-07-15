699231a5c864cc4499f756ef894b323d3a1ede77
unknown <izabellasilveir@gmail.com>
2020-07-15 17:35:11 -0300

    home.php e function

---
+++ home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+
+	<link rel="stylesheet" href="function/estilo.css" type="text/css">
+<!-- 	<?php
+		$css = file_get_contents('function/estilo.css');
+		echo $css;
+	?> -->
+</head>
+<body>
+	<div class="main">
+		<div id="cabecalho">
+			<div id="logo">
+				<a href=""><img id="image-logo" width="90" title="Efeirão" src="function/logo.png"></a>
+			</div>
+			<div class="search">	
+				<input type="text" id="pesquisa" name="pesquisa" placeholder="Pesquisar">
+				<button class="button" id="submit-search" type="submit"></button>
+			</div>	
+		</div>
+		<div id="user-space">
+			<div id="sell">
+				<input type="text" id="vender" name="vender" placeholder="Vender">
+			</div>
+			<div id="user">
+				<a href=""><img id="image-logo" width="90" title="Login" src="function/user.png"></a>
+			</div>
+			<div id="direct">
+				<a href=""><img id="image-logo" width="90" title="Direct" src="function/direct.jpg"></a>
+			</div>
+
+		</div>
+		
+	</div>
+</body>
+</html>
